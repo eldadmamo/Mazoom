6b49f6c838bb1f779f090cbbd9f742b31cd0ff4a
eldadstar <eldadfikre456@gmail.com>
2025-01-09 00:47:02 +0300

    updated

--- src/component/Login/EmailLogin.jsx
+++ src/component/Login/EmailLogin.jsx
@@ -62,7 +62,7 @@ const EmailLogin = ({onLogin }) => {
         <div className="w-full  lg:w-1/2 flex justify-center">
         {/* Right Image */}
            <div className='hidden md:block'>
-            <div className='flex flex-wrap gap-3'>
+            <div className='flex flex-wrap gap-3 ml-3'>
                 <button onClick={BacktoHome}><ArrowLeft/></button>
                 <h4>Back</h4>
             </div>
